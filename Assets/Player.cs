48f5ec3806095f55102d1a6ed738b98b23895335
brandon <lrbn86@gmail.com>
2017-06-27 13:24:29 -0400

    Add functionality to player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	public GameObject player;
+	public double playerHealth = 3000;
+	public float playerSpeed = 5.0f;
+	private float playerFireRate = 0.09f;
+	private float playerLastShot = 0.0f;
+	public double pelletDamage = 0.05;
+	public float pelletSpeed = 100.0f;
 
-	public float speed;
-	public Rigidbody rb;
+	public GameObject pellet;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody>();
-	}
+	public Transform pelletTopSpawn;
 
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
-		rb.AddForce (movement * speed);
+		playerMovement ();
+		playerShoot ();
 	}
 
-//	// Update is called once per frame
-//	void Update () {
-//
-//	}
+	void playerMovement() {
+		if (Input.GetKey("w") || Input.GetKey("up")) {
+			player.transform.Translate (Vector2.up * Time.deltaTime * playerSpeed);
+		}
+		if (Input.GetKey("s") || Input.GetKey("down")) {
+			player.transform.Translate (Vector2.down * Time.deltaTime * playerSpeed);
+		}
+		if (Input.GetKey("a") || Input.GetKey("left")) {
+			player.transform.Translate (Vector2.left * Time.deltaTime * playerSpeed);
+		}
+		if (Input.GetKey("d") || Input.GetKey("right")) {
+			player.transform.Translate (Vector2.right * Time.deltaTime * playerSpeed);
+		}
+	}
+
+	void playerShoot() {
+		if (Input.GetKey("space") && Time.time > playerFireRate + playerLastShot) {
+			GameObject shootUp = (GameObject)Instantiate (pellet, pelletTopSpawn.position, pelletTopSpawn.rotation);
+			Rigidbody2D up = shootUp.GetComponent<Rigidbody2D> ();
+			up.AddForce (Vector2.up * pelletSpeed);
+			playerLastShot = Time.time;
+		}
+	}
 }
