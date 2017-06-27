c401b408387f192cea5477e612d47f8ac945747b
brandon <lrbn86@gmail.com>
2017-06-27 13:03:03 -0400

    Upload Unity files

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+//	// Update is called once per frame
+//	void Update () {
+//
+//	}
+}
