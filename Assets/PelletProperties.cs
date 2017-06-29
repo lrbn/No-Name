79e29c53c8b0b28dbde86ebe467e9ee86906df3f
brandon <lrbn86@gmail.com>
2017-06-29 11:21:38 -0400

    Add shooting capability to player

---
+++ Assets/PelletProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletProperties : MonoBehaviour {
+
+	public float pelletLifeTime = 2.0f;
+
+	// Update is called once per frame
+	void Update () {
+		Destroy (this.gameObject, pelletLifeTime);
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -43,4 +43,5 @@ public class Player : MonoBehaviour {
 			playerLastShot = Time.time;
 		}
 	}
+
 }
